3045e4656e02a55623aa9a0282beb7244cda1d71
Lucas <lucasdubairedes@gmail.com>
2020-03-26 13:57:52 -0300

    Front-end da Aplicação

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -4,10 +4,14 @@ module.exports={
     async create(request, response){
         const {id} = request.body
 
-        const ong = await connection('ongs').where('id', id).select('name').first()
+        const ong = await connection('ongs')
+        .where('id', id)
+        .select('name')
+        .first()
 
         if(!ong){
-            return response.status(400).json()
+            return response.status(400).json({error:"Ong não encontrada por esse id"})
         }
+        return response.json(ong)
     }
 }
